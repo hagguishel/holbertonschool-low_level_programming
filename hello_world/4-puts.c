760151bfe220768629e608994f1b8c3a238aae83
hagguishel <hagguirzt@gmail.com>
2024-10-21 09:47:29 +0000

    C

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,13 @@
-#include<stdio.h> 
+#include <stdio.h>
+/**
+ * main it print out output using put
+ *
+ * Return: always (0)
+ *
+ */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle\n");
     return (0);
 }
 
