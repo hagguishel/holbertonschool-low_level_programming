d532253f8f16c2bdc72ded7d219d6c9ed3a61240
hagguishel <hagguirzt@gmail.com>
2024-10-23 12:29:15 +0000

    C

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**                                                                                                                                                                      
+* main - Entry point of the program                                                                                                                                      
+*                                                                                                                                                                        
+* Description: This function prints the alphabet in lowercase                                                                                                            
+* using the putchar function.                                                                                                                                            
+*                                                                                                                                                                        
+* Return: Always 0 (Success)                                                                                                                                             
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+if (letter != 'q' && letter != 'e')
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
