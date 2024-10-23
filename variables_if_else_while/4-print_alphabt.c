74959453db1ad8ce3fcf42b03bea9993a2928770
hagguishel <hagguirzt@gmail.com>
2024-10-23 12:32:05 +0000

    C

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**                                                                                                                                                                      
-* main - Entry point of the program                                                                                                                                      
-*                                                                                                                                                                        
-* Description: This function prints the alphabet in lowercase                                                                                                            
-* using the putchar function.                                                                                                                                            
-*                                                                                                                                                                        
-* Return: Always 0 (Success)                                                                                                                                             
+/**
+* main - Entry point of the program
+*
+* Description: This function prints the alphabet in lowercase
+* using the putchar function.
+*
+* Return: Always 0 (Success)
 */
 int main(void)
 {
