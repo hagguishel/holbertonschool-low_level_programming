f5817c95b29c0e9cf69007b2236856f075658b63
hagguishel <hagguirzt@gmail.com>
2024-10-25 12:24:05 +0000

    C

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints "_putchar", followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+write(1, "_putchar\n", 9);
+return (0);
+}
