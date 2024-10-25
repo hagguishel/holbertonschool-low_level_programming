9c5841af360ffd2bbf94e6690b1a8a95b53677b2
hagguishel <hagguirzt@gmail.com>
2024-10-25 14:00:55 +0000

    l

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+int ALP;
+int A;
+{
+for (A = '0'; A <= '9'; A++)
+{
+for (ALP = 'a'; ALP <= 'z'; ALP++)
+{
+_putchar(ALP);
+}
+_putchar('\n');
+}
+}
