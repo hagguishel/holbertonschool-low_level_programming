a80f14ff667e6b79ed2d0b03a9b6bbfbd6c40577
hagguishel <hagguirzt@gmail.com>
2024-10-25 11:31:44 +0000

    C

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+** main - Entry point of the program
+*
+* Description: This program prints all hexadecimal numbers in lowercase
+* from 0 to 9 and a to f, followed by a new line.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int base;
+for (base = 0; base < 10; base++)
+{
+putchar(base + '0');
+}
+putchar(',');
+putchar('\n');
+return (0);
+}
