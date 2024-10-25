ff9fdbd5f27534fb5798947f4aacb99cfd96720d
hagguishel <hagguirzt@gmail.com>
2024-10-25 09:55:38 +0000

    C

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-/*
- * Write a program that prints all the numbers of base 16
- * You can only use the putchar
- * All your code should be in the main function
- */
+/**
+** main - Entry point of the program
+*
+* Description: This program prints all hexadecimal numbers in lowercase
+* from 0 to 9 and a to f, followed by a new line.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 char A;
 for (A = 0 ; A <= 9 ; A++)
+{
 putchar('A' + 0);
+}
 for (A = 'a' ; A <= 'f' ; A++)
+{
 putchar ('A');
+}
 putchar ('\n');
 return (0);
 }
