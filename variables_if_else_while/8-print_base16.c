ec742f9296a616acf55daf85df22ec298a2ba0a4
hagguishel <hagguirzt@gmail.com>
2024-10-25 09:35:16 +0000

    C

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * Write a program that prints all the numbers of base 16
+ * You can only use the putchar
+ * All your code should be in the main function
+ */
+int main(void)
+{
+char A;
+for (A = 0 ; A >= 16 ; A++)
+{
+putchar ('A');
+}
+putchar ('\n');
+return (0);
+}
