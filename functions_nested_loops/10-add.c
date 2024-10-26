eae8307162e8b9570f7e072698cd62ca920f68d5
hagguishel <hagguirzt@gmail.com>
2024-10-26 14:51:43 +0000

    abs

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Ajoute deux entiers et retourne le résultat
+ * @a: Le premier entier
+ * @b: Le deuxième entier
+ *
+ * Return: La somme de a et b
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
