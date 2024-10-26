c2459a30cf7a321e50f53a9a6e72ec14d63e95d0
hagguishel <hagguirzt@gmail.com>
2024-10-26 14:19:30 +0000

    abs

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Affiche chaque minute de la journée de 00:00 à 23:59
+ */
+void jack_bauer(void)
+{
+int heures, minutes;
+for (heures = 0; heures < 24; heures++)
+{
+for (minutes = 0; minutes < 60; minutes++)
+{
+_putchar('0' + (heures / 10));
+_putchar('0' + (heures % 10));
+_putchar(':');
+_putchar('0' + (minutes / 10));
+_putchar('0' + (minutes % 10));
+_putchar('\n');
+}
+}
+}
