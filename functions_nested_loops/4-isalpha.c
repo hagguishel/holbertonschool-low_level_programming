f897283601ae5faf6007f780559d4e7d9b6de26b
hagguishel <hagguirzt@gmail.com>
2024-10-26 11:19:03 +0000

    put

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,17 +1,18 @@
 #include "main.h"
-/*
- * Fonction : _isalpha
- * Vérifie si le caractère passé en paramètre est une lettre de l'alphabet.
- * c : Le caractère à vérifier (sous forme d'un entier représentant la valeur ASCII).
- * Retourne : 1 si c est une lettre (minuscule ou majuscule), sinon 0.
- */
+
+/**
+_isalpha - Vérifie si un caractère est alphabétique (minuscule ou ma
+@c: Caractère |  v
+*
+Return: 1 si le caractère est une lettre alphabéti
+*/
 int _isalpha(int c)
 {
 int is_lower = (c >= 'a' && c <= 'z');
-int is_upper = (c >= 'A' && c <= 'Z');    
-if (is_lower || is_upper) 
+int is_upper = (c >= 'A' && c <= 'Z');
+if (is_lower || is_upper)
 {
-return 1;
+return (1);
 }
-return 0;
+return (0);
 }
