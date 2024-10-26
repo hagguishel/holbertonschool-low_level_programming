13c2a9adfe46cc65bca267e108fb9ceabb0a86f8
hagguishel <hagguirzt@gmail.com>
2024-10-26 13:35:23 +0000

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* _abs - Calcule la valeur absolue d'un entier
+* @n: L'entier à traiter
+*
+* Return: La valeur absolue de n
+*/
+int _abs(int n)
+{
+return (n >= 0 ? n : -n);
+}
