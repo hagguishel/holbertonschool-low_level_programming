c37e30f6bb67d4c2f40217456966ae28bccfd2f7
hagguishel <hagguirzt@gmail.com>
2024-10-26 13:50:38 +0000

    abs

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Affiche et retourne le dernier chiffre d'un nombre
+ * @n: Le nombre à traiter
+ *
+ * Return: Le dernier chiffre du nombre
+ */
+int print_last_digit(int n)
+{
+int last_digit;
+last_digit = n % 10;
+if (last_digit < 0)
+last_digit = -last_digit;
+_putchar('0' + last_digit);
+return last_digit;
+}
