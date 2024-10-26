61e1b4275a8f4684abfffd18b45ce671391b37e3
hagguishel <hagguirzt@gmail.com>
2024-10-26 10:24:26 +0000

    putchar

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,15 +1,10 @@
 #include "main.h"
 
-/*
- * _islower - Checks if a character is a lowercase letter.
- * @c: The character to check.
- *
- * This function returns 1 if the character is a lowercase letter 
- * (between 'a' and 'z'), and 0 otherwise.
- *
- * Note: The call to _putchar is unnecessary in this function and 
- * does not contribute to its functionality.
- */
+/**
+*_islower-Checks if a character is lowercase.
+*@c:Character to check
+*Return:1 if the character is lowercase, 0 otherwise.
+*/
 int _islower(int c)
 {
 if (c >= 'a')
