817994f73bdc47aad6ec477470a787f105c47b40
hagguishel <hagguirzt@gmail.com>
2024-10-28 14:29:03 +0000

    functions_nested_loops

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,13 @@
 #include "main.h"
 
+/**
+ * print_most_numbers - Affiche les chiffres de 0 à 9, sauf 2 et 4
+ * Return: Aucun retour (void).
+ */
 void print_most_numbers(void)
 {
 int i;
-for (int i = 0; i <= 9; i++)
+for (i = 0; i <= 9; i++)
 {
 if (i != 2 && i != 4)
 {
