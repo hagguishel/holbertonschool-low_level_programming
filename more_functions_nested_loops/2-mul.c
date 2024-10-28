b8f236f4d8ec9e9bdf0be7d7c41c8a9beba3fe11
hagguishel <hagguirzt@gmail.com>
2024-10-28 13:45:18 +0000

    functions_nested_loops

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplie deux entiers.
+ * @a: Le premier entier.
+ * @b: Le deuxième entier
+ * Return: Le produit de a et b.
+ */
+int mul(int a, int b)
+{
+int resultat = a * b;
+return (resultat);
+}
