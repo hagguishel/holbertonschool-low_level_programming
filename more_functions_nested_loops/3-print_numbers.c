5d4ae84f331ba5ac9433fa4246ba108ac7233c85
hagguishel <hagguirzt@gmail.com>
2024-10-28 13:51:49 +0000

    functions_nested_loops

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - Affiche les chiffres de 0 à 9.
+ * Return: Aucun retour (void).
+ */
+void print_numbers(void)
+{
+int A;
+for (A = 0; A <= 9; A++)
+_putchar (A + '0');
+_putchar ('\n');
+return (0);
+}
