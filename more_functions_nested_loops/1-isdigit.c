a1ca31ac5b9df755fc803032b7c00b33d710beb6
hagguishel <hagguirzt@gmail.com>
2024-10-28 13:23:06 +0000

    functions_nested_loops

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- * _isdigit - Vérifie si un caractère est un chiffre (0 à 9).
- * @c: Caractère à vérifier (code ASCII).
- *
+ * _isdigit - Vérifie si un caractère est un chiffre (0 à 9)
+ * @c: Caractère à vérifier (code )
  * Return: 1 si `c` est un chiffre, 0 sinon.
  */
 
-int _isdigit(int c);
+int _isdigit(int c)
 {
-int A = '0';
-for (; A > 10; A++)
+int A;
+for (; A <= '9'; A++)
 {
 if (A == c)
 	return (1);
