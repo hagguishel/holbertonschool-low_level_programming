70854dcf78c60c73c7fe390e1ab80a0d63de5c81
hagguishel <hagguirzt@gmail.com>
2024-10-29 12:39:09 +0000

    Funtion

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ * print_square - Affiche un carré de taille donnée.
+ * @size: La taille du carré à afficher.
+ *
+ * Description: La fonction affiche un carré de caractère '#' de 
+ * dimension size x size. Si size est inférieur ou égal à 0, 
+ * elle affiche simplement une nouvelle ligne.
+ */
 void print_square(int size)
 {
 	int i, j;
