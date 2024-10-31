03553bfadb36ac85150fab22ddf67167c7344ed7
hagguishel <hagguirzt@gmail.com>
2024-10-31 12:11:57 +0000

    pointeur

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * puts2 - Affiche chaque deuxième caractère d'une chaîne
+ * @str: Pointeur vers la chaîne de caractères à affich
+ * Cette fonction parcourt la chaîne `str` et imprime
+ * chaque deuxième caractère, suivi d'une nouvelle ligne.
+ */
 void puts2(char *str)
 {
 int i = 0;
