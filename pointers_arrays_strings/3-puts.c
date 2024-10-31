b0ae9931fc9c8e870123b14fcaeba900c66685c4
hagguishel <hagguirzt@gmail.com>
2024-10-31 09:18:20 +0000

    PointerA

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * _puts - Affiche une chaîne suivie d'une nouvelle ligne
+ * @str: La chaîne de caractères à affic
+ * Parcourt la chaîne `str` et affiche chaque caractère avec `_putchar`.
+ * Ajoute un saut de ligne après la chaîn
+ */
 void _puts(char *str)
 {
 int i;
