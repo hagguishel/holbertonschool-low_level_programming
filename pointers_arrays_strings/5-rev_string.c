953c33c66db475798f1dce2ce4d9956a8441b6e2
hagguishel <hagguirzt@gmail.com>
2024-10-31 10:20:38 +0000

    pointeur

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+#include "main.h"
+
+/**
+ * rev_string - Inverse une chaîne de caractères.
+ * @s: Pointeur vers la chaîne de caractères à inverser.
+ *
+ * Cette fonction échange les caractères de la chaîne de manière
+ * à inverser leur ordre.
+ */
+void rev_string(char *s)
+{
+int i, j;
+char temp;
+for (i = 0; s[i] != '\0'; i++)
+;
+for (j = 0; j < i / 2; j++)
+{
+temp = s[j];
+s[j] = s[i - j - 1];
+s[i - j - 1] = temp;
+}
+}
