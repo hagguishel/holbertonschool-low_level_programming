f9019d23b26bd7aded92f0a13c06a7b9cca961eb
hagguishel <hagguirzt@gmail.com>
2024-11-03 21:26:58 +0000

    pointeur2

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,42 +1,26 @@
 #include "main.h"
-
-/**
+/** 
  * _atoi - Converts a string to an integer.
- * @s: The string to be converted.
- *
- * Return: The integer value of the converted string.
+ * @s: The string to be converted.*
+ * Return: The integer value of the string.
  */
-int _atoi(char *s)
+int _atoi(char s)
 {
-    int i = 0;
-    int sign = 1;
-    unsigned int result = 0;
-    int has_number_started = 0;
-
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-' && !has_number_started)
-        {
-            sign *= -1;
-        }
-        else if (s[i] == '+' && !has_number_started)
-        {
-        }
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            has_number_started = 1;
-            result = result * 10 + (s[i] - '0');
-            if (result > 2147483647)
-            {
-                return (sign == 1) ? -1 : 0;
-            }
-        }
-        else if (has_number_started)
-        {
-            break;
-        }
-        i++;
-    }
-    return result * sign;
+int i = 0;
+int sign = 1;
+int result = 0;
+while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+{
+if (s[i] == '-')
+sign= -1;
+i++;
 }
-
+while (s[i] >= '0' && s[i] <= '9')
+{
+if (result > (2147483647 - (s[i] - '0')) / 10)
+return (sign == 1 ? 2147483647 : -2147483648);
+result = result * 10 + (s[i] - '0');
+i++;
+}
+return (result *sign);
+}
