d876857045923933b55d3649042d80dfd6aa3a70
hagguishel <hagguirzt@gmail.com>
2024-11-05 12:01:28 +0000

    pointeur

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stddef.h>
-
+/**
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: The main string to be scanned.
+ * @accept: The string containing the characters to match.
+ * Return: A pointer to the first occurrence in `s` of any of the
+ * bytes in `accept`, or NULL if no such byte is found.
+ */
 char *_strpbrk(char *s, char *accept)
 {
 int i, j;
