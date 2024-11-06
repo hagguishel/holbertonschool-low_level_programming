ee3f1c5d7606b7a237be4f0821827cde8a5a4e17
hagguishel <hagguirzt@gmail.com>
2024-11-06 09:28:23 +0000

    recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Imprime une chaîne en ordre inverse
+ * @s: Pointeur vers la chaîne de caractères à impri
+ * Description: Cette fonction utilise la récursivité pour parcour
+ * la chaîne de caractères jusqu'à sa fin, puis imprime chaque caractè
+ * dans l'ordre inverse lorsque la pile de récursion commence à se vide.
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
