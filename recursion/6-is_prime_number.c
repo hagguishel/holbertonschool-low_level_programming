37853e7b7d5da62601d564715a52bed02b408ec4
hagguishel <hagguirzt@gmail.com>
2024-11-07 13:58:27 +0000

    recursion

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -4,48 +4,29 @@
 /**
  * is_prime_number - Vérifie si un nombre est premier.
  * @n: Le nombre à tester.
+ * @i est la pour faire beau
  * Return: 1 si le nombre est premier, sinon 0.
  */
-int foncrion_deux(int n , i);
-
+int foncrion_deux(int n, int i);
 int is_prime_number(int n)
 {
-	int i = 2;
-	if (n < 1)
-	{	
-		return (0);
-	}
-	if (n == 2 || n == 3)
-	{
-		return (1);
-	}
-	return (foncrion_deux(int n , x));
-
-
-
-	int foncrion_deux(int n , x)
-	{
-	
-	if (n == i)
-	return (1);
-	
-       if (n % i == 0)
-       return (0)
-	       if ( i > n)
-	return (1);
-         int foncrion_deux (n , i + 1);
-	}
-
-
-
-
-
-	
-	
-	
-	
-	
-	
-	
+if (n <= 1)
+return (0);
+if (n == 2 || n == 3)
+return (1);
+return (foncrion_deux(n, 2));
 }
-	
+/**
+ * foncrion_deux - Fonction auxiliaire pour vérifier si un nombre est premier
+ * @n: Le nombre à tester
+ * @i: Le diviseur potentiel pour vérifier si n est divisible
+ * Return: 1 si n est premier, sinon 0.
+ */
+int foncrion_deux(int n, int i)
+{
+if (n == i)
+return (1);
+if (n % i == 0)
+return (0);
+return (foncrion_deux(n, i + 1));
+}
