44d35424ed359240e923d67861919e1bc0ff80b2
hagguishel <hagguirzt@gmail.com>
2024-11-07 14:40:27 +0000

    recursion

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,13 +2,12 @@
 /**
  * main - Affiche le nom du programme.
  * @argc: Le nombre d'arguments (non utilisé ici).
- * @argv: Un tableau contenant les arguments, avec argv[0] comme nom du programme.
+ * @argv: Un tableau avec argv[0] comme nom du programme.
  * Return: Toujours 0.
  */
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
-	return (0);
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
 }
-
