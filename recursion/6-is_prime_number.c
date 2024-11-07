ebdb82da1f5fc10486705ded9e3ba1c7f17701a6
hagguishel <hagguirzt@gmail.com>
2024-11-07 13:24:07 +0000

    recursion

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * is_prime_number - Vérifie si un nombre est premier.
+ * @n: Le nombre à tester.
+ * Return: 1 si le nombre est premier, sinon 0.
+ */
+int foncrion_deux(int n , i);
+
+int is_prime_number(int n)
+{
+	int i = 2;
+	if (n < 1)
+	{	
+		return (0);
+	}
+	if (n == 2 || n == 3)
+	{
+		return (1);
+	}
+	return (foncrion_deux(int n , x));
+
+
+
+	int foncrion_deux(int n , x)
+	{
+	
+	if (n == i)
+	return (1);
+	
+       if (n % i == 0)
+       return (0)
+	       if ( i > n)
+	return (1);
+         int foncrion_deux (n , i + 1);
+	}
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+}
+	
