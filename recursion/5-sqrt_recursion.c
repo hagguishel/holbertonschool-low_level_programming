285f5dd882be67d8b33a4f570e25ff94a104dd9f
hagguishel <hagguirzt@gmail.com>
2024-11-07 11:30:08 +0000

    recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,6 +1,10 @@
 #include "main.h"
 
-int _sqrt_helper(int n, int x);
+/**
+ * _sqrt_recursion - Calcule la racine carrée d'un nombre donn�
+ * @n: Le nombre pour lequel on cherche la racine carrée
+ * Return: La racine carrée de `n` si elle existe, sinon -1 si `n` est négatif
+ */
 int _sqrt_recursion(int n)
 {
 	int x = 1;
@@ -11,15 +15,21 @@ int _sqrt_recursion(int n)
 	return (_sqrt_helper(n, x));
 }
 
+/**
+ * _sqrt_helper - Fonction auxiliaire pour calculer la racine carrée
+ * @n: Le nombre pour lequel on cherche la racine carrée
+ * @x: L'entier qui est testé pour être la racine carrée de `
+ * Return: La racine carrée de `n` si elle existe, sinon -.
+ */
 int _sqrt_helper(int n, int x)
 {
-	 if (x * x == n)
-        {
-                return (x);
-        }
-        if (x * x > n)
-        {
-                return (-1);
-        }
-	return _sqrt_helper(n, x + 1);
+	if (x * x == n)
+	{
+		return (x);
+	}
+	if (x * x > n)
+	{
+		return (-1);
+	}
+	return (_sqrt_helper(n, x + 1));
 }
