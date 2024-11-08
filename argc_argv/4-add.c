7c3eaf8f16a954026dc5ec10b1bac44c79aba723
hagguishel <hagguirzt@gmail.com>
2024-11-08 17:28:10 +0000

    Arg

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 /**
- * main - Prints all arguments it receives
- * @argc: The number of arguments passed to the program
- * @argv: An array of strings representing the arguments passed to the program
+ * main - Additionne des nombres positifs passés en arguments.
+ * @argc: Nombre d'arguments passés au programme
+ * @argv: Tableau de chaînes représentant les argument
  *
- * Return: Always 0 (Success)
+ * Return: 0 si succès, 1 si un des arguments n'est pas un nombre
  */
 int main(int argc, char *argv[])
 {
-int i;
-for (i = 0; i < argc; i++)
+int sum = 0;
+int i, j;
+if (argc == 1)
 {
-printf("%s\n", argv[i]);
+printf("0\n");
+return (0);
 }
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
 return (0);
 }
