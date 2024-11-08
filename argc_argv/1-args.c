6c6a3e46a3c6a75eda40e702fc320698b884d7ed
hagguishel <hagguirzt@gmail.com>
2024-11-08 09:07:40 +0000

    argv

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * @argc: The count of arguments passed to the program
+ * @argv: The array of arguments (not used here)
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+return 0;
+}
--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -2,16 +2,16 @@
 
 /**
  * main - Affiche tous les arguments reçus par le programme
- * @argc: Le nombre d'arguments passés au programme
- * @argv: Un tableau contenant les arguments sous forme de chaînes de caractère
- * Return: Toujours 0 (indique que le programme s'est terminé correctement)
+ * @argc: Le nombre d'arguments passés au programm
+ * @argv: Un tableau contenant les arguments sous forme de chaînes de caractè
+ * Return: Toujours 0
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	for (i = 0; i < argc; i++)
-	{
-	printf("%s\n", argv[i]);
-	}
-	return (0);
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
 }
