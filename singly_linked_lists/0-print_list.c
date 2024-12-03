b280df4d354110c72caf21044e29af1fc5d443b0
hagguishel <hagguitzt@gmail.com>
2024-12-03 10:15:54 +0000

    singly

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "lists.h"
-
+#include <stdlib.h>
 /**
  * print_list - Affiche tous les éléments d'une liste chaînée
  * @h: Pointeur vers le premier nœud de la liste
@@ -14,9 +14,13 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		printf("[0] (nil)\n");
+		{
+			printf("[0] (nil)\n");
+		}
 		else
-		printf("[%u] %s\n", h->len, h->str);
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		count++;
 	}
