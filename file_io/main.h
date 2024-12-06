232f77fbb779b8cc0b8932acc74cd869664e2fe7
hagguishel <hagguirzt@gmail.com>
2024-12-06 13:35:56 +0000

    file

--- file_io/main.h
+++ file_io/main.h
@@ -1,9 +1,8 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stddef.h>
-#include <stddef.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
