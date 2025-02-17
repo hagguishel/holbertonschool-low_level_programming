2d4b9a00c58e50b59c78044ac214968a066cddfb
hagguishel <hagguirzt@gmail.com>
2025-02-17 09:09:53 +0000

    TT

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Prints a programming quote
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
