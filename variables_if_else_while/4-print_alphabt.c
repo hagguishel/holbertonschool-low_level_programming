cb091fb8599894f012d4d984412d41073c2b7b30
hagguishel <hagguirzt@gmail.com>
2025-02-17 15:46:43 +0000

    TT

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints a programming quote
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter ++)
+	if (letter != 'e' && letter != 'q')
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
