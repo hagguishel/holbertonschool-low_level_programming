7619a430d2197377cbc4a9cfa6ac1ec8a6e2bddf
hagguishel <hagguirzt@gmail.com>
2025-02-17 09:17:35 +0000

    TT

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints a programming quote
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
