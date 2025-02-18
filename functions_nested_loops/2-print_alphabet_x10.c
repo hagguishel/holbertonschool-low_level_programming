88a681377157fd46899474b8507ddda191fd0e24
hagguishel <hagguirzt@gmail.com>
2025-02-18 14:14:21 +0000

    TT

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@ void print_alphabet_x10(void)
 {
 	int letter, count;
 
-	for (count = 0; count <= 10; count++)
+	for (count = 0; count < 10; count++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
