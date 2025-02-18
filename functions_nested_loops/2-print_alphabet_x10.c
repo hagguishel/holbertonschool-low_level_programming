7c7e4169c6fe991790fcc1e29b23630964517968
hagguishel <hagguirzt@gmail.com>
2025-02-18 14:12:50 +0000

    TT

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int letter, count;
+
+	for (count = 0; count <= 10; count++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
