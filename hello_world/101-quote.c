5e0045ca0f99cccd23f62939836d1d98f60d3301
hagguishel <hagguirzt@gmail.com>
2025-02-23 11:23:03 +0000

    TT

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,5 +1,12 @@
 #include <unistd.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: Prints - Dora Korpar, 2015-10-19"
+ *              to the standard error output.
+ *
+ * Return: Always 1 (Error code)
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
