6459c99c36ef895f5ab4838541c6b567444bd151
hagguishel <hagguishel@gmail.com>
2025-02-24 10:53:47 +0000

    TT

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,19 @@
 #include "main.h"
+
 /**
- * void more_numbers - Affiche les chiffres 
+ * more_numbers - Affiche les chiffres de 0 à 14, 10 fois
  */
 void more_numbers(void)
 {
 	int i, j;
 
-	for (j = 0; j <= 10; j++)
+	for (j = 0; j < 10; j++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			if (i < 9)
-			{
-			
-				_putchar((i % 10) + '0');
-			}
-			else
-			{
-				_putchar((i / 10) + '0'); 
-				_putchar((i % 10) + '0');
-			}
+			if (i >= 10)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 		}
 	_putchar('\n');
 	}
