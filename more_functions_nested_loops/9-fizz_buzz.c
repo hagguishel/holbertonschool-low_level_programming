bdb2123099cf4023fac89047452125bca705fc6e
hagguishel <hagguirzt@gmail.com>
2025-02-25 10:07:28 +0000

    TT

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * main - Programme qui affiche les nombres de 1 à 100.
+ *        Remplace les multiples de 3 par "Fizz",
+ * Return: 0 (Succès)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
