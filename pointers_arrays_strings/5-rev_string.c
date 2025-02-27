8f877e054879e34bec699de13184310841ead613
hagguishel <hagguishel@gmail.com>
2025-02-27 13:41:33 +0000

    TT

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,15 +1,18 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * rev_string - Fonction qui inverse une chaîne de caractères.
- * @s: Pointeur vers la chaîne de caractères |  inverser.
+ * @s: Pointeur vers la chaîne de caractères à inverser.
  *
- * Cette fonction modifie la chaîne en place sans utilise mémoire supplémeni
+ * Cette fonction modifie
  */
+
 void rev_string(char *s)
 {
 	int i = 0;
 	int j;
+	char temp;
 
 	while (s[i] != '\0')
 	{
@@ -19,7 +22,7 @@ void rev_string(char *s)
 
 	for (i = 0; i < j; j--)
 	{
-		char temp = s[i];
+		temp = s[i];
 		s[i] = s[j];
 		s[j] = temp;
 		i++;
