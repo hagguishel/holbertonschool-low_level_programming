04cdd816aa445ea20c2070079ff5462beee6fbde
hagguishel <hagguirzt@gmail.com>
2025-03-02 17:56:51 +0000

    TT

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,38 +1,24 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * _strncat - Concatène les n premiers caractères de s de dest
- * @dest: La chaîne de caractè les n premiers caractères src ont ajoue
- * @src: La chaîne de caractères dont les nremiers caractères seront ajout�
- * @n: Le nombre maximal de caractères de sr concatener
- *
- * Description:
- * fonction ajoute les n premiers caractères de la chaîne `s'fin de la chaine
- * Si `src` contient moins de n caractères, tous les caractères de `srcajoute
- * Après la concaténation, un caractère null ('\0') est ajo a la fin de dest
- *
- * Return: La chaîne `dest` après l'ajout den premiers caractères de `src`.
+ * _strncat - Concatenate at most n bytes from src to dest.
+ * @dest: Destination string.
+ * @src: Source string.
+ * @n: Number of bytes to concatenate.
+ * Return: Pointer to dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
+	char *ptr = dest;
 
-	i = 0;
-	j = 0;
+	while (*dest != '\0')
+		dest++;
 
-	while (dest[i] != '\0')
+	while (n > 0 && *src)
 	{
-		i++;
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
 	}
-
-	while (src[j] != '\0' && j < n)
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
-	return (dest);
+	return (ptr);
 }
