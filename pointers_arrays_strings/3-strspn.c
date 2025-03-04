7680f0eb458e50648d5186593eca18dbe9cc19c4
hagguishel <hagguirzt@gmail.com>
2025-03-04 09:33:11 +0000

    TT

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,22 +10,19 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, count = 0;
-	int found;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		found = 0;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
-				found = 1;
+				count++;
 				break;
 			}
 		}
-		if (!found)
-			break;
-		count++;
+		if (accept[j] == '\0')
+		break;
 	}
 	return (count);
 }
