31327216a669c4bf95f932e712c626d8bbf54deb
hagguishel <hagguirzt@gmail.com>
2025-03-04 11:33:01 +0000

    TT

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,16 +1,24 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: The string to be searched.
+ * @accept: The set of characters to match.
+ *
+ * Return: Pointer to the first occurrence of any character from accept
+ *         in the string s, or NULL if no match is found.
+ */
 char *_strpbrk(char *s, char *accept)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if (s[i] == accept[j])
-            return (s + i);
-        }
-    }
-    return (NULL);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+	return (NULL);
 }
--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,20 +1,27 @@
 #include "main.h"
-
+/**
+ * _strstr - Locates a substring in a string
+ * @haystack: The main string to search in
+ * @needle: The substring to find
+ *
+ * Return: Pointer to the beginning of the located substring,
+ *         or NULL if the substring is not found.
+ */
 char *_strstr(char *haystack, char *needle)
 {
-    int b1, b2;
+	int b1, b2;
 
-    for (b1 = 0; haystack[b1] != '\0'; b1++)
-    {
-        for (b2 = 0; needle[b2] != '\0'; b2++)
-        {
-            if (haystack[b1 + b2] != needle[b2])
-            break;
-        }
-        if (needle[b2] == '\0')
-        {
-            return (haystack + b1);
-        }
-    }
-    return (NULL); 
+	for (b1 = 0; haystack[b1] != '\0'; b1++)
+	{
+		for (b2 = 0; needle[b2] != '\0'; b2++)
+		{
+			if (haystack[b1 + b2] != needle[b2])
+				break;
+		}
+		if (needle[b2] == '\0')
+		{
+			return (haystack + b1);
+		}
+	}
+	return (NULL);
 }
