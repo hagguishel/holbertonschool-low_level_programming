f2bcbbaecb5a4584a600565786315ecd153af863
hagguishel <hagguirzt@gmail.com>
2025-03-06 13:32:29 +0100

    TT

--- recursion/main.h
+++ recursion/main.h
@@ -16,7 +16,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _putchar(char c);
 int square_root(int x, int y);
-int check_prime(int n, int i);
+int find_helper(int a, int diviseur);
 
 
 #endif
