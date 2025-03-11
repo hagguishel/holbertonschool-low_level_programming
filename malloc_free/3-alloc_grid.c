0068d4eb2370d9ac444eba9eb7f4ca3e1682940c
hagguishel <hagguirzt@gmail.com>
2025-03-11 15:10:49 +0100

    TT

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
  * alloc_grid - Alloue un tableau 2D de dimensions width x height.
  * @width: Nombre de colonnes.
@@ -8,7 +10,7 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i;
+	int i, y;
 	int **grille;
 
 	if (width <= 0 || height <= 0)
@@ -16,17 +18,28 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	grille = malloc(height * sizeof(int*));
+	grille = malloc(height * sizeof(int *));
 	if (!grille)
 	{
 		return (NULL);
 	}
+
 	for (i = 0; i < height; i++)
 	{
 		grille[i] = malloc(width * sizeof(int));
 		if (!grille[i])
 		{
+			for (y = 0; y < i; y++)
+			{
+				free(grille[y]);
+			}
+			free(grille);
 			return (NULL);
+		}
+
+		for (y = 0; y < width; y++)
+		{
+			grille[i][y] = 0;
 		}
 	}
 	return (grille);
