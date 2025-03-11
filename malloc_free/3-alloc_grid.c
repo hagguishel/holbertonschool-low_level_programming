60b8f587b31906edf9af8321201508271c565705
hagguishel <hagguirzt@gmail.com>
2025-03-11 14:54:48 +0100

    TT

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * alloc_grid - Alloue un tableau 2D de dimensions width x height.
+ * @width: Nombre de colonnes.
+ * @height: Nombre de lignes.
+ *
+ * Return: Pointeur vers le tableau 2D ou NULL si l'allocation échoue.
+ */
 int **alloc_grid(int width, int height)
 {
 	int i;
