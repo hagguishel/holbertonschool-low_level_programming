4a17eb7fc9826e49ee00a54b911cd6e1b7f4c9bd
hagguishel <hagguirzt@gmail.com>
2025-03-12 11:32:20 +0100

    TT

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * free_grid - Libère une grille bidimensionnelle précédemme allouée par a
+ * @grid: Le p vers la grille
+ * @height: Le nomb de lignes dans la grile.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
