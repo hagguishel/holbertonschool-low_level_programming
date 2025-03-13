fbc74309f6c8ac7d0980746f9ec1dc4fd9766ad6
hagguishel <hagguirzt@gmail.com>
2025-03-13 11:26:28 +0100

    TT

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -13,15 +13,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *tab;
 
 	if (nmemb == 0 || size == 0)
-	return (NULL);
+		return (NULL);
 
 	tab = malloc(size *nmemb);
 	if (!tab)
-	return (NULL);
+		return (NULL);
 
 	ptr = (char *)tab;
+
 	for (i = 0; i < nmemb * size; i++)
-	ptr[i] = 0;
+		ptr[i] = 0;
 
 	return (tab);
 }
