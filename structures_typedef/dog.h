53a7921611dd08dcd772f7907e32f02bcc482ca8
hagguishel <hagguirzt@gmail.com>
2025-03-13 14:31:17 +0100

    TT

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,17 +1,22 @@
-#ifndef MYHEADER_H
-#define MYHEADER_H
+#ifndef DOG_H
+#define DOG_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-
+/**
+ * struct dog - A simple structure to represent a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
