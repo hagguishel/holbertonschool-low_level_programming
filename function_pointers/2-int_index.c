877fc22a78d3a68765d1c535952c1141d30cf713
hagguishel <hagguirzt@gmail.com>
2025-03-18 15:22:02 +0100

    TT

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size - 1; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+		    return (i);
+		}
+	    }
+
+	return (0);
+}
