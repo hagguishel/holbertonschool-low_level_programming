fa9282ba2c2e7b6de495ad51c0499aa19d258b01
hagguishel <hagguirzt@gmail.com>
2025-03-20 14:04:15 +0100

    TT

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -13,15 +13,15 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int result = 0;
 
-	if (n == 0 )
-		return (0);
+	if (n == 0)
+	return (0);
 
-	va_start (arg, n);
+	va_start(arg, n);
 	for (i = 0; i < n; i++)
 	{
-		result += va_arg (arg, unsigned int);
+		result += va_arg(arg, unsigned int);
 	}
-	va_end (arg);
+	va_end(arg);
 
 	return (result);
 }
---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list num;
+	unsigned int i, result = 0;
+
+	if (n == 0)
+	return (0);
+
+	va_arg (arg, n) 
+}
