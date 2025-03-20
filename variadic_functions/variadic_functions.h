278fb4801a1f3baa34ea6800beac2656e94829fa
hagguishel <hagguirzt@gmail.com>
2025-03-20 13:50:54 +0100

    TT

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,17 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
+
+#endif
