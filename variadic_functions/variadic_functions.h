a6963d59b3a2f273d857afdc7a08977e390132e5
hagguishel <hagguirzt@gmail.com>
2025-03-21 13:05:33 +0100

    TT

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,17 +1,24 @@
-#ifndef MYHEADER_H
-#define MYHEADER_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <string.h>
-#include <stddef.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int _putchar(char c);
+
+/**
+ * struct printer - associates a type with a print function
+ * @type: the type of argument (e.g., 'c', 'i', 'f', 's')
+ * @func: pointer to the function that prints the type
+ */
+
+typedef struct printer
+{
+	char type;
+	void (*func)(va_list args, char *separator);
+} printer_t;
 
 #endif
