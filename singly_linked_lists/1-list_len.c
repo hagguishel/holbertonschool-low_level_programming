cacd0eae6fc65b7bac03935c7d494adbf16bb14a
hagguishel <hagguirzt@gmail.com>
2025-04-02 17:21:48 +0200

    TT

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-
+/**
+ * list_len - Returns the number of elements of a list_t list
+ * @h: Pointer to the head of the list
+ *
+ * Return: The number of elements in the list
+ */
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
