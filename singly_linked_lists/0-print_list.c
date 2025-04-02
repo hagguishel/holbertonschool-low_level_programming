47f4abfde9319b0b3027920e7afe75211c81915e
hagguishel <hagguirzt@gmail.com>
2025-04-02 17:08:17 +0200

    TT

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,5 +2,18 @@
 
 size_t print_list(const list_t *h)
 {
-	
+	size_t count = 0;
+
+	while  (h != NULL)
+	{
+		if (h->str == NULL)
+		printf("[0] (nil)");
+
+		else
+		printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
